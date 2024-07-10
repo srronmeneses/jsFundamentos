f902e6a6908d0068165212e5ce027faa4aada30c
sr.ronmeneses <sr.ronmeneses@gmail.com>
2024-07-10 11:28:57 -0400

    palabras reservadas y nombres de variables

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,36 @@
+/*note: Las palabras reservadas son palabras que tienen un uso específico, solo deben
+ser utilizadas para las que fueron creadas caso contrario podrían darte un error*/
+
+/*note: Revisar el PDF proporcionado en clase para ver algunas de las palabras reservaras 
+para el lenguaje de JS*/
+// let const = 123;
+// console.log(const);
+
+/*bug: No se debe de utilizar caracteres raros o especiales de algún idioma para declarar variables
+recomendamos revisar los links para nombres validos:
+--> https://mothereff.in/js-variables
+--> https://mathiasbynens.be/notes/javascript-identifiers 
+*/
+// var Hͫ̆̒̐ͣ̊̄ͯ͗͏̵̗̻̰̠̬͝ͅE̴̷̬͎̱̘͇͍̾ͦ͊͒͊̓̓̐_̫̠̱̩̭̤͈̑̎̋ͮͩ̒͑̾͋͘Ç̳͕̯̭̱̲̣̠̜͋̍O̴̦̗̯̹̼ͭ̐ͨ̊̈͘͠M̶̝̠̭̭̤̻͓͑̓̊ͣͤ̎͟͠E̢̞̮̹͍̞̳̣ͣͪ͐̈T̡̯̳̭̜̠͕͌̈́̽̿ͤ̿̅̑Ḧ̱̱̺̰̳̹̘̰́̏ͪ̂̽͂̀͠ = 'Zalgo';
+// let ñ = 'ronald';
+// let variable.test = 123; No es valido colocar un punto en el nombre de variables
+
+/*TODO: El standard para crear variables son: */
+let _ = 123;
+let _variable = 'string';
+let $ = true;
+let objeto$ = Symbol('a');
+let conNumero123 = Math.PI;
+/*hack: Recomendaciones para asignar variables
+--> Tiene que ser algo claro y significativo
+--> Tiene que tener un sentido semántico  
+--> Si se tiene mas de dos palabras para una variable utilizar lowerCamelCase
+--> Si se tiene mas de dos palabras para un objeto, clase UpperCamelCase
+--> Utilizar el Underscore "_" para separa numero
+Ejm:*/
+let nombrePersonaGanadora = 'Peter Parker';
+
+class JuegoDelAnio{
+
+}
+
